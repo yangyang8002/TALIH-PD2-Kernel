47ceb5582039d4f8dbe41efd7e773efd6d41da8f
yangyang8002 <yangyang8002@users.noreply.github.com>
2026-08-16 16:41:17 +0800

    susfs: port clean VFS hooks to 5.15 (dcache/proc_cmdline/proc_ns/uname)

--- fs/dcache.c
+++ fs/dcache.c
@@ -32,6 +32,10 @@
 #include <linux/bit_spinlock.h>
 #include <linux/rculist_bl.h>
 #include <linux/list_lru.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+#include <linux/susfs_def.h>
+extern bool susfs_is_inode_sus_path(struct inode *inode);
+#endif
 #include "internal.h"
 #include "mount.h"
 
@@ -2351,6 +2355,10 @@ seqretry:
 				continue;
 			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 				continue;
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+			if (dentry->d_inode && susfs_is_inode_sus_path(dentry->d_inode))
+				continue;
+#endif
 		}
 		*seqp = seq;
 		return dentry;
@@ -2433,6 +2441,11 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 
 		if (dentry->d_name.hash != hash)
 			continue;
+
+#ifdef CONFIG_KSU_SUSFS_SUS_PATH
+		if (dentry->d_inode && susfs_is_inode_sus_path(dentry->d_inode))
+			continue;
+#endif
 
 		spin_lock(&dentry->d_lock);
 		if (dentry->d_parent != parent)
