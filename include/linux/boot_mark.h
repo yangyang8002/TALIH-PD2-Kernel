a06867d04c4325af66c6c4dd609589658d9735ab
yangyang8002 <yangyang8002@users.noreply.github.com>
2026-08-28 21:41:51 +0800

    debug round16: bisect early-param token 5 entry (stack_depot/kasan/kvm/cgroup batch done?)

--- include/linux/boot_mark.h
+++ include/linux/boot_mark.h
@@ -8,5 +8,5 @@ void boot_crash(const char *s);
 
 /* Self-destruct bisection inside do_early_param(): crash after this many
  * early-param cmdline tokens have been fully handled. Bump each round. */
-#define EARLY_PARAM_BISECT_N 1
+#define EARLY_PARAM_BISECT_N 5
 #endif
